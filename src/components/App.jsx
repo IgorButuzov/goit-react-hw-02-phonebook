2a8648c7550b9fd88cf02cccc2fe686eefe8e965
Igor Butuzov <ibutuzov87@gmail.com>
2023-02-22 16:30:51 +0200

    update phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,47 @@
-export const App = () => {
-  return (
-    <div>
-      task-2/ phonebook
+import React from "react";
+
+import css from 'components/App.module.css'
+
+import Form from 'components/ContactForm/ContactForm'
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  formSubmitHandler = data =>{
+    setTimeout(() => {
+      console.log('я в Апп');
+    }, 2000);
+    
+  }
+  
+  render() {
+    return (
+    <div className={css.phoneBook}>
+      <div>
+      <h1>Phonebook</h1>
+      <Form 
+      onSubmit={this.formSubmitHandler}
+      />
+      </div>
+      <div>
+      <h2>Contacts</h2>
+      <label className={css.label}>Find contacts by name
+      <input
+        type="text"
+        name="find"
+      />
+      </label>
+      <ul>
+        <li>Vasya Pypkin</li>
+      </ul>
+      </div>
     </div>
   );
 };
+}
+
+export default App;
+
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import { nanoid } from 'nanoid'
+
+import css from 'components/App.module.css'
+
+
+class Form extends React.Component {
+  state = {
+    name: '',
+    number: ''
+  }
+  
+  nameid = nanoid(3);
+
+  handleInputChange = e =>{
+    const {name,value} = e.currentTarget;
+    this.setState({[name]:value});
+  }
+  handleSubmit = e =>{
+    e.preventDefault();
+    console.log(this.state);
+    this.resetForm();
+    this.props.onSubmit();
+  }
+  resetForm = () => {
+    this.setState({
+        name: '',
+        number: '', 
+    })
+  }
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <label className={css.label}>Name
+        <input
+          type="text"
+          name="name"
+          id={this.nameid}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={this.state.name}
+          onChange={this.handleInputChange}
+        />
+        </label>
+        <label className={css.label}>Number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={this.state.number}
+          onChange={this.handleInputChange}
+        />
+        </label>
+        <button type="submit">Add contact</button>
+        </form>
+  );
+};
+}
+
+export default Form;
